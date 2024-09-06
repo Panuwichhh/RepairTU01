b9b68c08702e97cb4983aa7b39a176f6711ebb33
Panuwichhh <panuwich2747@gmail.com>
2024-09-07 03:27:33 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import Test from './html/test.jsx'
+import Test from './page/test.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
