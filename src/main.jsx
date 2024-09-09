b5e4f7f2f12d87d0439cd6aaa22560added40c07
Panuwichhh <panuwich2747@gmail.com>
2024-09-10 02:16:26 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Nav from './component/Nav.jsx'
+import Nav from "./components/Nav.jsx";
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
