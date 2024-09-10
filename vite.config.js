1154e92d9c99bb1417848374dbc226c3c6cea210
Panuwichhh <panuwich2747@gmail.com>
2024-09-10 09:49:00 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: 'RepairTU01', // ระบุชื่อ repository ของคุณที่นี่
+  base: '/RepairTU01/', 
 })
