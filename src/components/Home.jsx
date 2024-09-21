29806f189d9eda127a0fe50709479f5dd6488ad4
Panuwichhh <panuwich2747@gmail.com>
2024-09-21 19:32:16 +0700

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,33 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <>
-     <div className="w-full h-[85%]  bg-[#F6F6F6] flex flex-wrap  items-center justify-center">
-        <div className="flex flex-col flex-wrap">
-            <div className="text-[#340000]">
-                <p className="font-bold text-[10vw] leading-none">WELCOME</p>
+     <div className="h-screen bg-[#F6F6F6] flex flex-cols-2  items-center justify-center">
+        <div className="">
+            <div className="text-[#340000] ml-5 ">
+                <p className="font-bold text-[10vw] text-center  leading-none mb-10">WELCOME</p>
                 <p className="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Error quaerat saepe consequatur esse excepturi unde?</p>
                 <p className="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     Error quaerat saepe consequatur esse excepturi unde?</p>
-                <p className="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Error quaerat saepe consequatur esse excepturi unde?</p>
-                <p className="intro">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Error quaerat saepe consequatur esse excepturi unde?</p>
-            </div>
 
+                
             <div className="text-[#340000] mt-10">
                 <h1 className="font-bold text-2xl ">How to use</h1>
                 <p className="intro">1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, libero!</p>
                 <p className="intro">2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident, molestiae.</p>
                 <p className="intro">3. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempore, dolorum.</p>
             </div>
+            </div>
         </div>
+        
 
-        <div className="flex flex-col justify-center">
-            <i classNameName="text-[15vw] text-[#340000] ml-5 fa-solid fa-screwdriver-wrench self-center"></i>
-            <button id="get_start" className="w-60 h-14 font-black bg-[#E20B0B] text-white rounded-3xl shadow-2xl mt-5 hover:opacity-80 hover:text-yellow-400">GET STARTED</button>
+        <div className="flex flex-col items-center">
+            <i class=" mb-20 text-[25vw] text-[#340000] fa-solid fa-screwdriver-wrench max-md:text-[20vw] "></i>
+            <button id="get_start" className="w-60 h-14 font-black bg-[#E20B0B] text-white rounded-3xl shadow-2xl mt-5 hover:opacity-80 hover:text-yellow-400"><Link to="/Upload">GET STARTED</Link></button>
         </div>
     </div>
     </>
